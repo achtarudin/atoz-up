509c2e754d761336ea76258d8fb45edebb26abdc
aru-gandsoft <aru@gandsoft.com>
2019-03-29 18:37:02 +0700

    setup sentinel model

---
+++ app/Models/Sentinel/activation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Sentinel;
+
+use Illuminate\Database\Eloquent\Model;
+
+class activation extends Model
+{
+    //
+}
---
+++ app/Models/Sentinel/persistence.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Sentinel;
+
+use Illuminate\Database\Eloquent\Model;
+
+class persistence extends Model
+{
+    //
+}
